9eaa0bafbc387f5513689e7b6cf1654a36c38c5b
priya <devipriya.akkineni123@gmail.com>
2024-04-23 15:34:22 +0530

    mycommit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import ShoeCard from "./ShoeCard";
+import PopularProductCart from "./PopularProductCart";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export {
+    Button,
+    Nav,
+    ShoeCard,
+    PopularProductCart,
+    ServiceCard,
+    ReviewCard,
+}
